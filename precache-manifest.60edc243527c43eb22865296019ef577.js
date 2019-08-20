c42cb3b477112c165a1dbdfe729de16ff05d0fef
Abhishek Sil <abhishek.sil@qyuki.com>
2019-08-20 14:03:11 +0530

    Updates

---
+++ precache-manifest.60edc243527c43eb22865296019ef577.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967b8f9babfc3457089c88b417a08cb0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3dcb801fb876c71779f1",
+    "url": "/monsters-rolodex/static/css/main.40f80c29.chunk.css"
+  },
+  {
+    "revision": "01d8317fe3a3835ac7c6",
+    "url": "/monsters-rolodex/static/js/2.f4bca13b.chunk.js"
+  },
+  {
+    "revision": "3dcb801fb876c71779f1",
+    "url": "/monsters-rolodex/static/js/main.3aa11c50.chunk.js"
+  },
+  {
+    "revision": "9ea9c29140c1f80c8044",
+    "url": "/monsters-rolodex/static/js/runtime~main.3810dc4c.js"
+  }
+]);
